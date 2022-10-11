bed10e351bd2e103763b21c43414633b3ba2e944
Petr <petrv2011@hotmail.com>
2022-10-11 16:52:58 +0300

    Task2_Lesson1

---
+++ Task2_Lesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2
+// НАПИСАТЬ ПРОГРАММУ КОТОРАЯ НА ВХОД ПРИНИМАЕТ 2 ЧИСЛА
+// и выдает , какое число большее, а какое меьшее
+Console.WriteLine("Задача 2");
+Console.Write("а=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b=");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b) 
+{
+    Console.WriteLine("max=" + a);
+} 
+else if (b > a) 
+{
+    Console.WriteLine("max=" + b);
+} 
+else if (a == b) 
+{
+    Console.WriteLine("a=b, " + a);
+}
