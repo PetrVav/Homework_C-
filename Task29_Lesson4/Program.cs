c9f5144778ae5be9d7b48aa5fbddf47c6a7816a6
Petr <petrv2011@hotmail.com>
2022-10-11 18:17:40 +0300

    Task29_Lesson4

---
+++ Task29_Lesson4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];  
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+                            
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 9); 
+
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]"); 
