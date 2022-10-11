a7a60fe516ae0a1c7a3dc444d8b64b666faea50c
Petr <petrv2011@hotmail.com>
2022-10-11 18:16:10 +0300

    Task27_Lesson4

---
+++ Task27_Lesson4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, 
+// которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int count =0; 
+if (n < 10)                               
+Console.WriteLine("Вы ввели одну цифру!");
+if (n < 0) 
+Console.WriteLine("Ошибка, попробуйте еще раз!");
+else                                   
+    while (n >= 1)
+
+    {
+    count += n % 10;
+    n = (n -n%10)/ 10;   
+    }
+Console.Write(count);
+
+
+
